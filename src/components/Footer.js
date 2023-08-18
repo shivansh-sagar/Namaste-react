dfeb4a994d08d0f0a2cc64bd89eff50545b5ada8
shivansh-sagar <shivanshsagar987@gmail.com>
2023-08-18 08:47:46 +0530

    full responsive nav bar is completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,10 @@ import "../style//Footer.css"
 const Footer= () =>{
     return (
         <>
-        <div className="footer-container h-{500px} border border-t-2 border-purple-500">
-            
+        <div className="footer-container flex items-center flex-col bg-slate-900 h-{500px} border border-t-2 text-white border--500">
+            <p>contact</p>
+            <p>email</p>
+            <p>social links</p>
 
         </div>
         </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,52 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../style/Header.css"
-import {MdOutlineFastfood} from 'react-icons/md'
+import { MdOutlineFastfood } from 'react-icons/md'
+import { HiMenuAlt4, HiX } from 'react-icons/hi'
 
 
 
-const Header =(props) =>{
-   
-  const [btnName, setbtnName]= useState("login");
-    return (
-      <div className='header shadow-md shadow-purple-3004 '>
-        <div className="logo-container">
-          <p><MdOutlineFastfood/>Moody Foody</p>
-          
-        </div>
-        <div className="nav-items">
-          <ul>
-           <Link className="nav-items-bars" to="/">Home</Link>
-           <Link className="nav-items-bars"  to="/about">AboutUs</Link>
-           <Link className="nav-items-bars"  to="/contact">Contact Us</Link>
-           <Link className="nav-items-bars"  to="/Grocery">Grocery</Link>
-            <button className="login-btn" onClick={()=>{ {props.changemode}
-              btnName==="login"?setbtnName("logout"):setbtnName("login")
-            }} >{btnName}</button>
-          </ul>
-        </div>
+const Header = (props) => {
+  const [toggle, setToggle] = useState(false)
+  // const [btnName, setbtnName] = useState("login");
+  return (
+    <nav className='app__navbar '>
+      <div className="app__navbar-logo">
+        <p><MdOutlineFastfood />Moody Foody</p>
+
       </div>
-    )
-  };
 
-  export default Header;
+      <ul className="app__navbar-links">
+        <Link className="nav-items-bars" to="/">Home</Link>
+        <Link className="nav-items-bars" to="/about">About</Link>
+        <Link className="nav-items-bars" to="/contact">Contact</Link>
+        <Link className="nav-items-bars" to="/Grocery">Grocery</Link>
+        {/* <button className="login-btn" onClick={() => {
+          { props.changemode }
+          btnName === "login" ? setbtnName("logout") : setbtnName("login")
+        }} >{btnName}</button> */}
+      </ul>
+
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} className="icon-x" />
+
+        {
+          toggle && (
+            <div>
+              <HiX onClick={() => setToggle(false)} />
+              <ul>
+                <Link className="link" onClick={() => setToggle(false)} to="/">home</Link>
+                <Link className="link" onClick={() => setToggle(false)} to="/about">about</Link>
+                <Link className="link" onClick={() => setToggle(false)} to="/contact">contact</Link>
+                <Link className="link" onClick={() => setToggle(false)} to="/grocery">Grocery</Link>
+              </ul>
+            </div>
+          )
+        }
+      </div>
+
+    </nav>
+  )
+};
+
+export default Header;
