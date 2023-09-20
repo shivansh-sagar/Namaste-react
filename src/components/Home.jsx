e3c4c3b121cf81dd80839dabb2f8226164ea1853
shivansh-sagar <shivanshsagar987@gmail.com>
2023-09-20 14:48:53 +0530

    creating home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,26 @@
 import { Link } from "react-router-dom";
-import {BsArrowReturnRight} from 'react-icons/bs'
-import "../style/Home.css"
-const Home= ()=>{
-    return(
-        <>
-        <div className="main flex flex-col items-center mb-5 p-5 h-full ">
-            <div className=" text-5xl" >
-                <h1 className="">let</h1>
-                <h1>Dive Deep</h1>
-                <h1>In the world of</h1>
-                <h1 className="mb-5">Food</h1>
-                <span><BsArrowReturnRight></BsArrowReturnRight><span className=" bg-green-400 rounded-lg uppercase p-2"><Link className="" to="/res">restaurant</Link></span></span>
-            </div>
-            
+import { BsArrowReturnRight } from "react-icons/bs";
+import "../style/Home.css";
+const Home = () => {
+  return (
+    <>
+      <div className="main flex flex-col items-center mb-5 p-5 h-full ">
+        <div className=" text-5xl">
+          <h1 className="">let</h1>
+          <h1>Dive Deep</h1>
+          <h1>In the world of</h1>
+          <h1 className="mb-5">Food</h1>
+          <span className="flex">
+            <BsArrowReturnRight></BsArrowReturnRight>
+            <span className=" bg-green-400 rounded-lg uppercase p-2">
+              <Link className="" to="/res">
+                restaurant
+              </Link>
+            </span>
+          </span>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 export default Home;
